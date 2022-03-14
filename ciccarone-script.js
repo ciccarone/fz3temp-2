d4d9d83d0b4d9458cd3a17063ff59ef29f4070fe
Tony Ciccarone <tonyvciccarone@gmail.com>
2022-03-14 18:52:41 -0400

    some non-database related hacky stuff

--- ciccarone-script.js
+++ ciccarone-script.js
@@ -19,6 +19,8 @@ jQuery(document).ready(function( $ ) {
 
   }
 
+
+
   if ($('.single-product').length > 0) {
 
     if (document.location.href.indexOf('sample') === -1){
@@ -28,6 +30,9 @@ jQuery(document).ready(function( $ ) {
         $('.entry-summary .wc-price-per-area').hide();
         $('.entry-summary .woocommerce-product-details__short-description p:first-child').hide();
         $('.entry-summary .woocommerce-product-details__short-description p:nth-child(2)').hide();
+        var sample_buy_link = $('.entry-summary .woocommerce-product-details__short-description p:last-child a').attr('href');
+        $('.entry-summary .woocommerce-product-details__short-description p:last-child').append('<div style="margin-top: 54px;"><a href="'+sample_buy_link+'" style="background-color: #ffb81c;    font-weight: bold;text-transform: uppercase;letter-spacing: 0.8px;padding: 15px 33px; color: white !important">ORDER NOW</a></div>');
+
     }
 
   }
@@ -46,12 +51,31 @@ jQuery(document).ready(function( $ ) {
           $('span', this).hide();
 
         } else {
+          $.fn.changeElementType = function(newType) {
+            var attrs = {};
+
+            $.each(this[0].attributes, function(idx, attr) {
+              attrs[attr.nodeName] = attr.nodeValue;
+            });
+
+            this.replaceWith(function() {
+              return $("<" + newType + "/>", attrs).append($(this).contents());
+            });
+          }
           var _href = $(this).attr("href");
           $(this).attr("href", _href.replace(/\/$/, "") + '-sample');
-
+          var new_href = $(this).attr("href");
+          console.log(new_href);
+          $(this).parent().parent().parent().parent().changeElementType('a');
+          $(this).parent().parent().parent().parent().attr({
+              "href": new_href,
+              // "border": "2px solid green"
+          });
         }
 
      });
   }
+
+
 
 });
